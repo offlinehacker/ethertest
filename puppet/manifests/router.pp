133f1bcf52c87ddb5d696d987cc129e555c0fe79
Matej Vrabec <vrabec.matej@gmail.com>
2013-01-04 14:18:36 +0100

    Added ipv6 to puppet and fab

--- puppet/manifests/router.pp
+++ puppet/manifests/router.pp
@@ -1,8 +1,22 @@
-define ifconfig($device, $changes) {
-    augeas  { "$name.config" :
+define ifconfig($device, $family, $changes, ) {
+    $ch =   [
+                "set auto[child::1 = '$device']/1 $device",
+                "set iface[last()+1] $device",
+                "set iface[last()]/family $family",
+                "set iface[last()]/method static",
+                ]
+    notice($ch)
+    augeas  { "ifconfig.$name.add" :
         context => "/files/etc/network/interfaces",
-        changes => $changes,
-        notify => Exec["$name.ifdown"]
+        changes => $ch,
+        onlyif => ["match iface[ . = '$device' and family ='$family'] size==0"]
+    }
+
+    augeas  { "ifconfig.$name" :
+        context => "/files/etc/network/interfaces/iface[ . = '$device' and family = '$family']",
+        changes =>  $changes,
+        notify => Exec["$name.ifdown"],
+        require => Augeas["ifconfig.$name.add"]
     }
 
     exec {"$name.ifdown":
@@ -22,15 +36,23 @@ class profile::router {
 
     ifconfig { "pub":
         device => "eth1",
+        family => "inet",
         changes => [
-                "set auto[child::1 = 'eth1']/1 eth1",
-                "set iface[. = 'eth1'] eth1",
-                "set iface[. = 'eth1']/family inet",
-                "set iface[. = 'eth1']/method static",
-                "set iface[. = 'eth1']/address 192.168.10.2",
-                "set iface[. = 'eth1']/netmask 255.255.255.0",
-                "set iface[. = 'eth1']/network 192.168.10.0",
-                "set iface[. = 'eth1']/gateway 192.168.10.1",
+                "set method static",
+                "set address 192.168.10.2",
+                "set netmask 255.255.255.0",
+                "set network 192.168.10.0",
+                "set gateway 192.168.10.1",
+            ],
+    }
+
+    ifconfig { "pubi_inet6":
+        device => "eth1",
+        family => "inet6",
+        changes => [
+                "set method static",
+                "set address 2001:db8::1",
+                "set netmask 64",
             ],
     }
 
