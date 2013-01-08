e67d5d30f79bfd0c4e64e1b1803fcf6f2a3df151
Matej Vrabec <vrabec.matej@gmail.com>
2013-01-08 22:33:05 +0100

    fixed dpkg puppet install incorrect version

--- update-puppet.sh
+++ update-puppet.sh
@@ -4,7 +4,7 @@ if [ ! -e ~/.bootstrap-vagrant ]; then
     cd /tmp
 
     wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb
-    dpkg -i puppetlabs-release-precise.deb
+    dpkg -i --force-all puppetlabs-release-precise.deb
     apt-get update && apt-get -y install puppet
 
     touch ~/.bootstrap-vagrant
